aeeb74298cd14993ab618a4a8a3a8724457fe137
enmanuel <enmanuellassis@gmail.com>
2021-11-16 18:33:49 -0400

    add routing dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Routes, Navigate } from 'react-router-dom';
+import { Route } from 'react-router';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Details from './components/Details';
 
 const App = () => (
   <>
     <Navbar />
-    <Home />
+    <Routes>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/details/:id">
+        <Details />
+      </Route>
+      <Route path="/details">
+        <Navigate to="/" />
+      </Route>
+    </Routes>
   </>
 );
 
